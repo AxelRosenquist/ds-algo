4ac9d11fcf98460c90e963148a4846e25243e8ae
Axel Rosenquist <Axel.Rosenquist4@gmail.com>
2025-01-03 13:20:21 +0100

    Added removal function

--- data-structure/linked-list/main.py
+++ data-structure/linked-list/main.py
@@ -8,7 +8,6 @@ class LinkedList:
     def __init__(self, head=None):
         self.head = head            
 
-
     def add(self, data):
         node = Node(data)
         if self.head is None:
@@ -25,21 +24,42 @@ class LinkedList:
     def print_list(self):
         current_node = self.head
         while current_node is not None:
-            print(current_node.data, current_node.link, '->' )
+            print(current_node.data, '->' )
             current_node = current_node.link
-        print('None')
+        print(f'None\n')
 
-  
+    def remove(self, to_remove):
+        prev_node, curr_node = Node(data=0, link=self.head), self.head
 
+        while curr_node:
+            next_node = curr_node.link
+            if curr_node.data == to_remove and curr_node==self.head:
+                self.head = next_node
+            elif curr_node.data == to_remove:
+                prev_node.link = next_node
+            else:
+                prev_node = curr_node
+
+            curr_node = next_node
+            
+        return
         
         
 
 
 linked_list = LinkedList()
-linked_list.print_list()
 linked_list.add(1)
-linked_list.print_list()
 linked_list.add(5)
-linked_list.print_list()
 linked_list.add(16)
+linked_list.add(19)
+linked_list.add(27)
+linked_list.print_list()
+
+linked_list.remove(to_remove=5)
+linked_list.print_list()
+
+linked_list.remove(to_remove=1)
+linked_list.print_list()
+
+linked_list.remove(to_remove=27)
 linked_list.print_list()
